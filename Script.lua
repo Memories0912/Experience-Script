b0f4d650ca909fe5829d25a5e96f7a02269361f1
Memories0912 <85147023+Memories0912@users.noreply.github.com>
2024-06-24 11:34:14 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 3101667897 then
   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Memories0912/loader/main/los.lua"))()
 elseif game.PlaceId == 9285238704 then
-  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Memories0912/loader/main/rc.lua"))() 
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Memories0912/loader/main/rc.lua"))()
+elseif game.PlaceId == 3623096087 then
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Memories0912/loader/main/ml.lua"))()
 end
